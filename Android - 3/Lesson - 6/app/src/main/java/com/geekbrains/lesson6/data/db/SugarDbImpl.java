9a3f93459bbc18cedf3502827f1198bcfb5b9dff
constant-i <constant-i@yandex.ru>
2019-10-03 00:59:24 +0300

    pреализация интерфейса DbProvider через Shugar

--- Android - 3/Lesson - 6/app/src/main/java/com/geekbrains/lesson6/data/db/SugarDbImpl.java
+++ Android - 3/Lesson - 6/app/src/main/java/com/geekbrains/lesson6/data/db/SugarDbImpl.java
@@ -1,4 +1,36 @@
 package com.geekbrains.lesson6.data.db;
 
-public class SugarDbImpl {
+
+import com.geekbrains.lesson6.domain.model.Article;
+import com.geekbrains.lesson6.data.entity.ArticleSugarData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SugarDbImpl implements DbProvider<ArticleSugarData, List<Article>> {
+    @Override
+    public void insert(ArticleSugarData data) {
+        data.save();
+    }
+
+    @Override
+    public void update(ArticleSugarData data) {
+        data.update();
+    }
+
+    @Override
+    public void delete(ArticleSugarData data) {
+        data.delete();
+    }
+
+    @Override
+    public List<Article> select() {
+        List<ArticleSugarData> articleSugarDataList = ArticleSugarData.listAll(ArticleSugarData.class);
+        List<Article> articleList = new ArrayList<>();
+        for (ArticleSugarData articleSugarData : articleSugarDataList) {
+            articleList.add(ArticleSugarData.convertToEntity(articleSugarData));
+        }
+        return articleList;
+    }
 }
